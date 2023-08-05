da75b639c4cf3c43b4e15689d5a0d1687fdf4431
karenabgaryan <karenabgaryan2005@gmail.com>
2023-08-05 11:30:51 -0700

    changing cors

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors({
-    origin: "https://bazaronline.onrender.com/",
+    origin: 'https://bazaronline.onrender.com',
     credentials: true,
-    optionsSuccessStatus: 20
+    optionsSuccessStatus: 200,
 }))
 
-app.use(express.static(path.join(__dirname, 'public/build')));
+// app.use(express.static(path.join(__dirname, 'public/build')));
 
 dotenv.config()
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
